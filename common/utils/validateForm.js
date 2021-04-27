7c81687e3b21d7cbe33dd0171c9777ea38248ad2
chen <chen18668070425@163.com>
2021-04-27 13:56:50 +0800

    身份证验证

--- common/utils/validateForm.js
+++ common/utils/validateForm.js
@@ -32,10 +32,33 @@ export default function validateForm(data, rule) {
 				return errorHandler('手机号格式不正确')
 			}
 		} else if (roleName === 'cardCode' || roleName === 'idCard') {
-			if (!data[roleName]) {
+			let identityNumber =  data[roleName]
+			if (!identityNumber) {
 				return errorHandler('身份证不能为空')
-			} else if (!RegExp.cardCode.test(data[roleName])) {
+			} else if (!RegExp.cardCode.test(identityNumber)) {
 				return errorHandler('身份证格式不正确')
+			}
+			//区位码校验
+			//出生年月日校验   前正则限制起始年份为1900;
+			let year = identityNumber.substr(6, 4), //身份证年
+			  month = identityNumber.substr(10, 2), //身份证月
+			  date = identityNumber.substr(12, 2), //身份证日
+			  time = Date.parse(month + "-" + date + "-" + year), //身份证日期时间戳date
+			  now_time = Date.parse(new Date()), //当前时间戳
+			  dates = new Date(year, month, 0).getDate(); //身份证当月天数
+			if (time > now_time || date > dates) {
+			  return errorHandler("出生日期不合规");
+			}
+			//校验码判断
+			var c = new Array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2); //系数
+			var b = new Array("1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2"); //校验码对照表
+			var id_array = identityNumber.split("");
+			var sum = 0;
+			for (var k = 0; k < 17; k++) {
+			  sum += parseInt(id_array[k]) * parseInt(c[k]);
+			}
+			if (id_array[17].toUpperCase() != b[sum % 11].toUpperCase()) {
+			  return errorHandler("身份证校验码不合规");
 			}
 		}
 	}
